b7d8a36713afecf3f92d55ada5cb1380fbe231aa
Dmitrii Verbeckii <verbeckii@gmail.com>
2023-04-26 21:57:43 +0300

    init swagger

--- apps/cloud-api/src/main.ts
+++ apps/cloud-api/src/main.ts
@@ -1,18 +1,23 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-
-import { AppModule } from "./app/app.module";
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
+  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloud-storage-API')
+    .setDescription('API DOCUMENTATION')
+    .setVersion('1.0')
+    .addTag('stores')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+  
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
