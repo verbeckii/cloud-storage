78eae5b40f29e28b1b3ab1b9b9deac9def406a4d
Dmitrii Verbeckii <verbeckii@gmail.com>
2023-04-29 18:47:11 +0300

    added queries lib and init user queries

---
+++ libs/backend/queries/src/lib/postgress/Users.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { prisma } from '@cloud-storage/backend/common/prisma-client';
+import { users as TUsers } from "@prisma/client";
+
+@Injectable()
+export class Users {
+
+  async qGetUsers() {
+    const users = await prisma.users.findMany();
+    return users;
+  }
+
+  async qGetUserByEmail(email: string) {
+    const user = await prisma.users.findFirst({
+      where: {
+        email,
+      },
+    });
+    return user;
+  }
+  async qGetUserById(id: number) {
+    const user = await prisma.users.findFirst({
+      where: {
+        id,
+      },
+    });
+    return user;
+  }
+
+  async qUpdateUser(data: TUsers) {
+    const user = await prisma.users.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    });
+    return user;
+  }
+
+  async qCreateUser(data: TUsers) {
+    const user = await prisma.users.create({
+      data,
+    });
+    return user;
+  }
+
+  async qDeleteUser(id: number) {
+    const user = await prisma.users.delete({
+      where: {
+        id,
+      },
+    })
+    return user;
+  }
+  
+}
